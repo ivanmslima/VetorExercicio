5c30b88d00f66feee314350372184e119cf78f59
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-21 20:25:23 -0300

    Adicionar arquivos de projeto.

---
+++ VetorExercicio/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+
+namespace exemplo {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Digite a quantidade de pessoas para calcular a altura e média de idade ");
+
+            int N;
+            N = int.Parse(Console.ReadLine());
+
+
+            string[] nomes = new string[N];
+            int[] idades = new int[N];
+            double[] alturas = new double[N];
+
+
+
+            //Leitura dos dados
+            for (int i = 0; i < N; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                nomes[i] = s[0];
+                idades[i] = int.Parse(s[1]);
+                alturas[i] = double.Parse(s[2], CultureInfo.InvariantCulture);
+            }
+
+            //Calculo de altura media
+            double soma = 0.0;
+            for (int i=0; i<N; i++)
+            {
+                soma = soma + alturas[i];
+            }
+            double media = soma / N;
+            Console.WriteLine("Altura média: " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+            //Porcentagem de pessoas abaixo de 16 anos
+            int cont = 0;
+            for (int i=0; i<N; i++)
+            {
+                if (idades[i] < 16)
+                {
+                    cont++;
+                }
+            }
+            double porcentagem = (double)cont / N * 100.0;
+            Console.WriteLine("Pessoas com menos de 16 anos: " + porcentagem.ToString("F1", CultureInfo.InvariantCulture) + "%");
+
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+}
